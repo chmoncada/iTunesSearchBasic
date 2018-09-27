d1ad0a2af7addeb8e815a238121ca23129474d5f
Charles Moncada <charles.moncada@emoshiapps.com>
2018-09-27 08:17:37 -0300

    Add `Song` model

---
+++ iTunesSearchBasic/Model/Song.swift
@@ -0,0 +1,18 @@
+//
+//  Song.swift
+//  iTunesSearchBasic
+//
+//  Created by Charles Moncada on 27/09/18.
+//  Copyright © 2018 Charles Moncada. All rights reserved.
+//
+
+import Foundation
+
+struct Song: Codable {
+	let artistName: String
+	let collectionName: String
+	let collectionId: Int
+	let trackName: String?
+	let artworkUrl100: String
+	let previewUrl: String?
+}
